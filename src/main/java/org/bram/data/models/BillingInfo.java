09d4ef8d280376ed46995ee2f8da0893e86d73e6
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-23 13:35:48 +0100

    first commit

--- src/main/java/org/bram/data/models/BillingInfo.java
+++ src/main/java/org/bram/data/models/BillingInfo.java
@@ -1,7 +1,13 @@
 package org.bram.data.models;
 
 import lombok.Data;
+import org.springframework.boot.autoconfigure.amqp.RabbitConnectionDetails;
 
 @Data
 public class BillingInfo {
+
+    private String receiverName;
+    private CreditCard card;
+    private Address deliveryAddress;
+    private String receiverPhoneNumber;
 }
