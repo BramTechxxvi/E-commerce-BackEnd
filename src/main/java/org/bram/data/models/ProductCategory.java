c2e112b3bce29fcf7c9587b716c0901c96c8d1a3
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-23 13:01:29 +0100

    first commit

--- src/main/java/org/bram/data/models/ProductCategory.java
+++ src/main/java/org/bram/data/models/ProductCategory.java
@@ -1,4 +1,17 @@
 package org.bram.data.models;
 
 public enum ProductCategory {
+
+    ELECTRONICS,
+    GROCERIES,
+    UTENSILS,
+    CLOTHING,
+    BOOKS,
+    FURNITURE,
+    GADGETS,
+    BEAUTY,
+    TOYS,
+    TOILETRIES
+
 }
+
