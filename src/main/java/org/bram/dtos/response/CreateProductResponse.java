da4bcb74cfea31659bbc1a59d73cb6159517db36
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-24 00:01:32 +0100

    first commit

--- src/main/java/org/bram/dtos/response/CreateProductResponse.java
+++ src/main/java/org/bram/dtos/response/CreateProductResponse.java
@@ -5,4 +5,6 @@ import lombok.Data;
 @Data
 public class CreateProductResponse {
 
+    private String productName;
+    private String productDescription;
 }
--- src/test/java/org/bram/data/repository/UserRepositoryTest.java
+++
@@ -1,7 +0,0 @@
-package org.bram.data.repository;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class UserRepositoryTest {
-
-}
