4bf92211b4ce1bef848cf0cbc11cd2f01619bef2
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-23 13:04:30 +0100

    first commit

--- src/main/java/org/bram/data/models/Product.java
+++ src/main/java/org/bram/data/models/Product.java
@@ -1,4 +1,16 @@
 package org.bram.data.models;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+@Data
 public class Product {
+
+    @Id
+    private String productId;
+    private String description;
+    private String productName;
+    private int price;
+    private ProductCategory category;
+
 }
