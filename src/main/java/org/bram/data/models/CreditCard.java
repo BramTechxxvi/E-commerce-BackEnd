286b1b3493097a75dd796578c592a7bbadbf94d9
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-23 13:44:42 +0100

    first commit

--- src/main/java/org/bram/data/models/CreditCard.java
+++ src/main/java/org/bram/data/models/CreditCard.java
@@ -4,4 +4,12 @@ import lombok.Data;
 
 @Data
 public class CreditCard {
+
+    private String cardNumber;
+    private String nameOnCard;
+    private String expirationMonth;
+    private String expirationYear;
+    private String cvv;
+    private CardType cardType;
 }
+
