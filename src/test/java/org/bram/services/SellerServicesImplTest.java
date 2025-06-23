cd468e1b996c5bc7098cbcc3abb6dcadf5790b68
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-24 00:03:43 +0100

    first commit

--- src/test/java/org/bram/services/SellerServicesImplTest.java
+++ src/test/java/org/bram/services/SellerServicesImplTest.java
@@ -1,6 +1,8 @@
 package org.bram.services;
 
 import org.bram.data.repository.SellerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,10 +10,12 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class SellerServicesImplTest {
 
+    @Autowired
     private SellerRepository sellerRepository;
+    Autowired
     private SellerServices sellerServices;
 
-    @B
+    @BeforeEach
     void setUp() {
         sellerRepository.deleteAll();
     }
