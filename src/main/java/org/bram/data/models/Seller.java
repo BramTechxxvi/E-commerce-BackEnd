282afab10135ec674fba5a6cc3d3fc0a0af23b55
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-23 15:34:05 +0100

    first commit

---
+++ src/main/java/org/bram/data/models/Seller.java
@@ -0,0 +1,9 @@
+package org.bram.data.models;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.Data;
+
+@Data
+@Document(collection="Sellers")
+public class Seller {
+}
