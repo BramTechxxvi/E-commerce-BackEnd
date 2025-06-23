3510915506b323b9e499ead7c8b7d3eb29cff0a1
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-23 20:37:50 +0100

    first commit

--- src/main/java/org/bram/services/CustomerServicesImpl.java
+++ src/main/java/org/bram/services/CustomerServicesImpl.java
@@ -1,27 +1,9 @@
 package org.bram.services;
 
-import org.bram.data.repository.CustomerRepository;
-import org.bram.dtos.request.ChangeEmailRequest;
-import org.bram.dtos.request.ChangePasswordRequest;
-import org.bram.dtos.response.ChangeEmailResponse;
-import org.bram.dtos.response.ChangePasswordResponse;
 
-public class CustomerServicesImpl implements UserServices{
+import org.springframework.stereotype.Service;
 
-    private CustomerRepository customerRepository;
+@Service
+public class CustomerServicesImpl {
 
-    @Override
-    public String getFullName() {
-        return
-    }
-
-    @Override
-    public ChangeEmailResponse changeEmail(ChangeEmailRequest request) {
-        return null;
-    }
-
-    @Override
-    public ChangePasswordResponse changePassword(ChangePasswordRequest request) {
-        return null;
-    }
 }
