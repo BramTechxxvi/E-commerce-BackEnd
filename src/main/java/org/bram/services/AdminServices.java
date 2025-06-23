436df46d35fabc98edb9b0d5b53ebe33dbd87bc2
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-24 00:08:44 +0100

    first commit

--- src/main/java/org/bram/services/AdminServices.java
+++ src/main/java/org/bram/services/AdminServices.java
@@ -1,5 +1,8 @@
 package org.bram.services;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface AdminServices {
 
 //    createProduct(product: Product): void
--- src/main/java/org/bram/services/SellerServices.java
+++ src/main/java/org/bram/services/SellerServices.java
@@ -1,5 +1,8 @@
 package org.bram.services;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface SellerServices {
 
     CreateProductResponse createProduct(CreateProductRequest request);
