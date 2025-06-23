c183bce8c717466719c688149cf84c9f2b79375a
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-23 20:42:29 +0100

    first commit

--- src/main/java/org/bram/services/SellerServicesImpl.java
+++ src/main/java/org/bram/services/SellerServicesImpl.java
@@ -1,11 +1,19 @@
 package org.bram.services;
 
+import org.bram.data.repository.SellerRepository;
 import org.bram.dtos.request.ChangeEmailRequest;
 import org.bram.dtos.request.ChangePasswordRequest;
 import org.bram.dtos.response.ChangeEmailResponse;
 import org.bram.dtos.response.ChangePasswordResponse;
 
 public class SellerServicesImpl implements UserServices{
+
+    private SellerRepository sellerRepository;
+
+    public SellerServicesImpl(SellerRepository sellerRepository) {
+        this.sellerRepository = sellerRepository;
+    }
+
     @Override
     public ChangeEmailResponse changeEmail(ChangeEmailRequest request) {
         return null;
