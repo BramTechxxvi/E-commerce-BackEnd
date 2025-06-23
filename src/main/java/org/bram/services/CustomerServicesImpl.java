eabe01e0484976892bba0048adc0cc3a14d0b8f4
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-23 20:34:11 +0100

    first commit

--- src/main/java/org/bram/services/CustomerServicesImpl.java
+++ src/main/java/org/bram/services/CustomerServicesImpl.java
@@ -1,11 +1,14 @@
 package org.bram.services;
 
+import org.bram.data.repository.CustomerRepository;
 import org.bram.dtos.request.ChangeEmailRequest;
 import org.bram.dtos.request.ChangePasswordRequest;
 import org.bram.dtos.response.ChangeEmailResponse;
 import org.bram.dtos.response.ChangePasswordResponse;
 
 public class CustomerServicesImpl implements UserServices{
+
+    private CustomerRepository customerRepository;
 
     @Override
     public String getFullName() {
