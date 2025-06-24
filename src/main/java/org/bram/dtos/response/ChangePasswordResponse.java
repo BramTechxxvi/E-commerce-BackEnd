143822594c20431b77c85fc2dd64333d1984bc6f
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-24 16:47:08 +0100

    first commit

--- src/main/java/org/bram/dtos/response/ChangePasswordResponse.java
+++ src/main/java/org/bram/dtos/response/ChangePasswordResponse.java
@@ -1,9 +1,10 @@
 package org.bram.dtos.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-
+@AllArgsConstructor
 public class ChangePasswordResponse {
 
     private String message;
