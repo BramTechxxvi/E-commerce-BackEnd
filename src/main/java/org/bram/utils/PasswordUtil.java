dce7ae80753fe1efcd4a6197d86d37d31b8521f0
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-24 11:50:50 +0100

    first commit

--- src/main/java/org/bram/utils/PasswordUtil.java
+++ src/main/java/org/bram/utils/PasswordUtil.java
@@ -1,4 +1,12 @@
 package org.bram.utils;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 public class PasswordUtil {
+
+    public static String hashPassword(String password) {
+        return BCrypt.withDefaults().hashToString(12, password.toCharArray());
+    }
+
+
 }
