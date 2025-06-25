98e3f8e2aa6a8ed78b9cab7f668bf5a3846800d0
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-25 11:37:05 +0100

    first commit

--- src/main/java/org/bram/data/repository/AuthenticationRepository.java
+++
@@ -1,9 +0,0 @@
-package org.bram.data.repository;
-
-import org.bram.dtos.response.LoginResponse;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthenticationRepository extends MongoRepository<LoginResponse, String> {
-}
