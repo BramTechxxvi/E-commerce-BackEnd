aa55ad9f19a341692feaaa6c3f3e8b1d73366341
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-26 12:46:14 +0100

    first commit

--- src/main/java/org/bram/configuration/CloudinaryConfig.java
+++ src/main/java/org/bram/configuration/CloudinaryConfig.java
@@ -1,5 +1,19 @@
 package org.bram.configuration;
 
-@
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class CloudinaryConfig {
+
+    @Bean
+    public Cloudinary cloudinary() {
+        return new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", "dld9a4wcn",
+                "api_key", "419937922268742",
+                "api_secret", "3q8AbRNjsPXg--BN7vpngpnrypI"
+        ));
+    }
 }
