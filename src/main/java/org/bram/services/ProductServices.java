c729d4faf9f516f952d14735b1c36db95c56b41c
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-26 13:12:52 +0100

    first commit

--- src/main/java/org/bram/services/ProductServices.java
+++ src/main/java/org/bram/services/ProductServices.java
@@ -1,4 +1,11 @@
 package org.bram.services;
 
+import org.bram.dtos.request.CreateProductRequest;
+import org.bram.dtos.response.CreateProductResponse;
+
 public interface ProductServices {
+
+    CreateProductResponse createProduct(CreateProductRequest request);
+
+    RemoveProduct
 }
