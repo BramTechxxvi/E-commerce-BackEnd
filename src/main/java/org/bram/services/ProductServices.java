02e87f129b91807554684356c26db363115c778d
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-26 13:22:22 +0100

    first commit

--- src/main/java/org/bram/services/ProductServices.java
+++ src/main/java/org/bram/services/ProductServices.java
@@ -1,6 +1,7 @@
 package org.bram.services;
 
 import org.bram.dtos.request.CreateProductRequest;
+import org.bram.dtos.request.RemoveProductRequest;
 import org.bram.dtos.response.ApiResponse;
 import org.bram.dtos.response.CreateProductResponse;
 
