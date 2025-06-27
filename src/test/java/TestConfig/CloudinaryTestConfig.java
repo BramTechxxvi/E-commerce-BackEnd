99dae4fa0ae3193c8ac9601543778b30a9efa57c
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-27 13:46:58 +0100

    test-confit commit

--- src/test/java/TestConfig/CloudinaryTestConfig.java
+++ src/test/java/TestConfig/CloudinaryTestConfig.java
@@ -1,5 +1,16 @@
 package TestConfig;
 
-@TEST
+import com.cloudinary.Cloudinary;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import static org.mockito.Mockito.mock;
+
+@TestConfiguration
 public class CloudinaryTestConfig {
+
+    @Bean
+    public Cloudinary cloudinary() {
+        return (mock(Cloudinary.class));
+    }
 }
