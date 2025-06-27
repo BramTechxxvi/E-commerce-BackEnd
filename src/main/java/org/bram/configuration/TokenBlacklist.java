ec32f533a507ab2fbe3b739cd690782ff6498e80
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-27 10:12:35 +0100

    first commit

--- src/main/java/org/bram/configuration/TokenBlacklist.java
+++ src/main/java/org/bram/configuration/TokenBlacklist.java
@@ -2,6 +2,19 @@ package org.bram.configuration;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
 @Component
 public class TokenBlacklist {
+
+    private final Set<String> blackistedTokens = ConcurrentHashMap.newKeySet();
+
+    public void getBlackistedTokens(String token) {
+        blackistedTokens.add(token);
+    }
+
+    public boolean isTokenBlacklisted(String token) {
+        return blackistedTokens.contains(token);
+    }
 }
