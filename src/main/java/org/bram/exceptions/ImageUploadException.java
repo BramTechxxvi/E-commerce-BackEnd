33e96eff023ec4b9cf31616687bd510ff736e144
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-30 17:50:47 +0100

    deleteProduct

--- src/main/java/org/bram/exceptions/ImageUploadException.java
+++ src/main/java/org/bram/exceptions/ImageUploadException.java
@@ -1,4 +1,7 @@
 package org.bram.exceptions;
 
 public class ImageUploadException extends DetailsAlreadyInUseException{
+    public ImageUploadException(String message) {
+        super(message);
+    }
 }
