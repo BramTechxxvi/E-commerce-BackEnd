5d4b469cb3ce858e1ff6124089bb0ecb056f0195
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-30 18:04:54 +0100

    admin

--- src/main/java/org/bram/data/repository/AdminRepository.java
+++ src/main/java/org/bram/data/repository/AdminRepository.java
@@ -1,4 +1,9 @@
 package org.bram.data.repository;
 
-public interface AdminRepository {
+import org.bram.data.models.Admin;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminRepository extends MongoRepository<Admin, String> {
 }
