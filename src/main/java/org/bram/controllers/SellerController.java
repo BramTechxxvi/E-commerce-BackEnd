e98ff6aa1062294e1de6aca85004038171b9b0fb
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-01 17:01:12 +0100

    controllers

--- src/main/java/org/bram/controllers/SellerController.java
+++ src/main/java/org/bram/controllers/SellerController.java
@@ -1,9 +1,19 @@
 package org.bram.controllers;
 
+import org.bram.dtos.request.ChangeEmailRequest;
+import org.bram.dtos.response.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/sellers")
 public class SellerController {
+
+    @PostMapping("/changeEmail")
+    public ResponseEntity<ApiResponse> changeEmail(@RequestBody ChangeEmailRequest request) {
+        return null;
+    }
 }
