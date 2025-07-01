cc4d135bdd7330d688320462977d812269b3a4bd
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-01 17:14:18 +0100

    controllers

--- src/main/java/org/bram/controllers/SellerController.java
+++ src/main/java/org/bram/controllers/SellerController.java
@@ -1,7 +1,13 @@
 package org.bram.controllers;
 
+import jakarta.validation.Valid;
 import org.bram.dtos.request.ChangeEmailRequest;
 import org.bram.dtos.response.ApiResponse;
+import org.bram.exceptions.UserNotFoundException;
+import org.bram.services.SellerServicesImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,8 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/sellers")
 public class SellerController {
 
-    @PostMapping("/changeEmail")
-    public ResponseEntity<ApiResponse> changeEmail(@RequestBody ChangeEmailRequest request) {
-        return null;
+    private final SellerServicesImpl sellerServices;
+
+    @Autowired
+    public SellerController(SellerServicesImpl sellerServices) {
+        this.sellerServices = sellerServices;
     }
+
+    @PostMapping("/changeEmail")
+    public ResponseEntity<ApiResponse> changeEmail(@RequestBody @Valid ChangeEmailRequest request) {
+        try {
+            ApiResponse response = sellerServices.changeEmail(request);
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+
+        } catch (UserNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body( new )
+        }
 }
