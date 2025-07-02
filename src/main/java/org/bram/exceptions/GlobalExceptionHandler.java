e0bf63a9ba269bebb11c3218663ba815f8031e97
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-02 13:43:44 +0100

    global-Exception

--- src/main/java/org/bram/exceptions/GlobalExceptionHandler.java
+++ src/main/java/org/bram/exceptions/GlobalExceptionHandler.java
@@ -10,18 +10,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(AccessDeniedException.class)
-    public ResponseEntity<ApiResponse> handleProductNotFound(AccessDeniedException ex) {
-        return buildResponse(ex.getMessage(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<ApiResponse> handleAccessDenied(AccessDeniedException ex) {
+        return buildResponse(ex.getMessage(), HttpStatus.FORBIDDEN);
     }
 
+    @ExceptionHandler(DetailsAlreadyInUseException.class)
+    public ResponseEntity<ApiResponse> handleDetailsAlreadyInUse(DetailsAlreadyInUseException ex) {
+        return buildResponse(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(ImageUploadException.class)
+    public ResponseEntity<ApiResponse> handleImageUpload(ImageUploadException ex) {
+        return buildResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 
 
     private ResponseEntity<ApiResponse> buildResponse(String message, HttpStatus status) {
         return ResponseEntity.status(status).body(new ApiResponse(message, false));
     }
 
-    // === HELPER ===
-//    private ResponseEntity<ApiResponse> buildResponse(String message, HttpStatus status) {
-//        return ResponseEntity.status(status).body(new ApiResponse(message, false));
-//    }
 }
