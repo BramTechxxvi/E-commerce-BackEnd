3e0962ccae0a57f670def8fd569e3eb25c19a6c9
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-03 11:34:24 +0100

    admin-services

--- src/main/java/org/bram/services/AdminServicesImpl.java
+++ src/main/java/org/bram/services/AdminServicesImpl.java
@@ -1,8 +1,18 @@
 package org.bram.services;
 
+import org.bram.data.repository.UserRepository;
 import org.bram.dtos.response.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class AdminServicesImpl implements AdminServices{
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public AdminServicesImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public ApiResponse banUser(String id) {
         return null;
