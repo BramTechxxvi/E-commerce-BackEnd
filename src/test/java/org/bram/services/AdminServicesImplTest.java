3cf2ff7a95ade76e8d100805fb0601efd2f79098
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-03 13:16:01 +0100

    admin-services

--- src/test/java/org/bram/services/AdminServicesImplTest.java
+++ src/test/java/org/bram/services/AdminServicesImplTest.java
@@ -1,7 +1,15 @@
 package org.bram.services;
 
+import org.bram.TestConfig.CloudinaryTestConfig;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@ActiveProfiles("test")
+@Import(CloudinaryTestConfig.class)
 class AdminServicesImplTest {
 
 }
