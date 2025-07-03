4f8455a55e02e68b7d6d880206ba3469a8c2d763
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-03 19:40:37 +0100

    admin-services

--- src/main/java/org/bram/controllers/AdminController.java
+++ src/main/java/org/bram/controllers/AdminController.java
@@ -2,6 +2,12 @@ package org.bram.controllers;
 
 import org.bram.dtos.response.ApiResponse;
 import org.bram.services.AdminServicesImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,28 +17,23 @@ public class AdminController {
 
     private final AdminServicesImpl adminServices;
 
+    @Autowired
     public AdminController(AdminServicesImpl adminServices) {
         this.adminServices = adminServices;
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping("/ban/{id}")
-    public ResponseEntity<ApiResponse> banUser
+    public ResponseEntity<ApiResponse> banUser(@PathVariable("id") String id) {
+        ApiResponse response = adminServices.banUser(id);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
 
-
-//        /**
-//         * Only accessible to logged-in users with ADMIN role
-//         */
-//        @PreAuthorize("hasRole('ADMIN')")
-//        @PostMapping("/ban/{userId}")
-//        public ResponseEntity<ApiResponse> banUser(@PathVariable String userId) {
-//            ApiResponse response = adminServices.banUser(userId);
-//            return ResponseEntity.ok(response);
-//        }
-//
-//        @PreAuthorize("hasRole('ADMIN')")
-//        @PostMapping("/unban/{userId}")
-//        public ResponseEntity<ApiResponse> unbanUser(@PathVariable String userId) {
-//            ApiResponse response = adminServices.unBanUser(userId);
-//            return ResponseEntity.ok(response);
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @PostMapping("/unBan/{id}")
+    public ResponseEntity<ApiResponse> unBanUser(@PathVariable("id") String id) {
+        ApiResponse response = adminServices.unBanUser(id);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
 
 }
