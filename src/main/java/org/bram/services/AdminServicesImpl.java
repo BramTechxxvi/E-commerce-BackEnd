aa329917351752f62636b4035d941462cbc1f5af
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-03 11:32:53 +0100

    admin-services

--- src/main/java/org/bram/services/AdminServicesImpl.java
+++ src/main/java/org/bram/services/AdminServicesImpl.java
@@ -1,4 +1,15 @@
 package org.bram.services;
 
-public class AdminServicesImpl imple{
+import org.bram.dtos.response.ApiResponse;
+
+public class AdminServicesImpl implements AdminServices{
+    @Override
+    public ApiResponse banUser(String id) {
+        return null;
+    }
+
+    @Override
+    public ApiResponse unBanUser(String id) {
+        return null;
+    }
 }
