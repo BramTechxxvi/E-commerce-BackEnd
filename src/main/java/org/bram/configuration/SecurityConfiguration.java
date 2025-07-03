e2dd48f5d2f8c1a20ae79534beb3ad5398215568
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-03 13:13:26 +0100

    admin-services

--- src/main/java/org/bram/configuration/SecurityConfiguration.java
+++ src/main/java/org/bram/configuration/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -14,6 +15,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfiguration {
 
     private final JwtService jwtService;
@@ -37,9 +39,9 @@ public class SecurityConfiguration {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers("/seller/**").hasRole("SELLER")
-                        .requestMatchers("/customer/**").hasRole("CUSTOMER")
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/seller/**").hasAuthority("SELLER")
+                        .requestMatchers("/customer/**").hasAuthority("CUSTOMER")
+                        .requestMatchers("/admin/**").hasAuthority("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(
--- src/main/java/org/bram/services/AdminServicesImpl.java
+++ src/main/java/org/bram/services/AdminServicesImpl.java
@@ -67,6 +67,6 @@ public class AdminServicesImpl implements AdminServices{
                 seller.setBanned(false);
                 sellerRepository.save(seller);
             });
-        }
+        } return new ApiResponse("User unbanned succesfully", true);
     }
 }
