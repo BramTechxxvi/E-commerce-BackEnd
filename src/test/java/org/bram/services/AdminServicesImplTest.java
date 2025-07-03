11741e48b9651334974f5a4c16dbeb04b87e8f94
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-03 14:29:44 +0100

    admin-services

--- src/test/java/org/bram/services/AdminServicesImplTest.java
+++ src/test/java/org/bram/services/AdminServicesImplTest.java
@@ -1,6 +1,13 @@
 package org.bram.services;
 
 import org.bram.TestConfig.CloudinaryTestConfig;
+import org.bram.data.repository.AdminRepository;
+import org.bram.data.repository.UserRepository;
+import org.bram.dtos.request.*;
+import org.bram.dtos.response.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
@@ -12,4 +19,48 @@ import static org.junit.jupiter.api.Assertions.*;
 @Import(CloudinaryTestConfig.class)
 class AdminServicesImplTest {
 
+    @Autowired
+    private AdminServicesImpl adminServices;
+    @Autowired
+    private AdminRepository adminRepository;
+    @Autowired
+    private UserRepository userRepository;
+    private RegisterRequest registerRequest;
+    private RegisterResponse registerResponse;
+    private LoginRequest loginRequest;
+    private LoginResponse loginResponse;
+
+
+    @BeforeEach
+    void setUp() {
+        adminRepository.deleteAll();
+        userRepository.deleteAll();
+    }
+
+    @Test
+    public void adminCanBanAUser__banUserTest() {
+
+    }
+
+    private void registerCustomer() {
+        registerRequest.setFirstName("John");
+        registerRequest.setLastName("Doe");
+        registerRequest.setEmail("john@doe.com");
+        registerRequest.setPassword("password111");
+        registerRequest.setPhone("090373542523");
+        registerRequest.setUserRole("CUSTOMER");
+
+        registerResponse = authenticationService.register(registerRequest);
+    }
+
+    private void registerSeller() {
+        registerRequest.setFirstName("Grace");
+        registerRequest.setLastName("Ayoola");
+        registerRequest.setEmail("grace@ayoola.com");
+        registerRequest.setPassword("password111");
+        registerRequest.setPhone("090373542529");
+        registerRequest.setUserRole("SELLER");
+
+        registerResponse = authenticationService.register(registerRequest);
+    }
 }
