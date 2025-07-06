57604acbef26e13ced10b07ee1766ec482d9876c
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-06 07:49:47 +0100

    cart-services

--- src/test/java/org/bram/services/CartServicesImplTest.java
+++ src/test/java/org/bram/services/CartServicesImplTest.java
@@ -1,9 +1,14 @@
 package org.bram.services;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@ActiveProfiles("test")
+@Import
 class CartServicesImplTest {
 
 
