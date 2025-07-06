891b3d9274451e9345ace6ff209075af804e5797
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-06 07:51:59 +0100

    cart-services

--- src/test/java/org/bram/services/CartServicesImplTest.java
+++ src/test/java/org/bram/services/CartServicesImplTest.java
@@ -1,8 +1,10 @@
 package org.bram.services;
 
 import org.bram.TestConfig.CloudinaryTestConfig;
+import org.bram.data.repository.CartRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,6 +14,12 @@ import static org.junit.jupiter.api.Assertions.*;
 @Import(CloudinaryTestConfig.class)
 class CartServicesImplTest {
 
+    private CartRepository cartRepository;
+    private CartServicesImpl cartServices;
+
+    @BeforeEach() {
+
+    }
 
     @Test
     public void addAnItemToCart__addItemTest() {
