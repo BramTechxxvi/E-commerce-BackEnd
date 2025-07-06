ba54ed4aded505f9d9ceb981134d189ff553a0a2
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-06 07:06:23 +0100

    admin-services

--- src/main/java/org/bram/data/repository/CartRepository.java
+++ src/main/java/org/bram/data/repository/CartRepository.java
@@ -1,5 +1,9 @@
 package org.bram.data.repository;
 
+import org.bram.data.models.ShoppingCart;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
 @Repository
-public interface CartRepository {
+public interface CartRepository extends MongoRepository<ShoppingCart, String> {
 }
