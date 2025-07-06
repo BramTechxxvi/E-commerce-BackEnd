fc167ab3dd5b6fceeaaf0028889b9a1cbddceb1e
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-06 17:08:27 +0100

    cart-services

--- src/main/java/org/bram/data/models/ShoppingCart.java
+++ src/main/java/org/bram/data/models/ShoppingCart.java
@@ -1,11 +1,17 @@
 package org.bram.data.models;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
 @Data
+@Document("ShoppingCarts")
 public class ShoppingCart {
 
+    @Id
+    private String id;
+    private String userId;
     private List<Item> items;
 }
--- src/main/java/org/bram/dtos/request/RemoveItemRequest.java
+++ src/main/java/org/bram/dtos/request/RemoveItemRequest.java
@@ -1,7 +1,9 @@
 package org.bram.dtos.request;
 
+import lombok.Data;
 import org.bram.data.models.Product;
 
+@Data
 public class RemoveItemRequest {
 
     private int quantity;
