7d1fa26afc20e489b48fe91ed81dce2bf7d2f8be
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-06 07:45:39 +0100

    cart-services

--- src/main/java/org/bram/services/CartServicesImpl.java
+++ src/main/java/org/bram/services/CartServicesImpl.java
@@ -1,9 +1,15 @@
 package org.bram.services;
 
+import org.bram.dtos.request.AddItemRequest;
+import org.bram.dtos.response.ApiResponse;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CartServicesImpl {
+public class CartServicesImpl implements CartServices{
 
 
+    @Override
+    public ApiResponse addItem(AddItemRequest request) {
+        return null;
+    }
 }
